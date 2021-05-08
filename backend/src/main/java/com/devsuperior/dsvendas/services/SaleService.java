15a7eb24d5c0212066785e3f544af07bd8232d87
daniel <eng.daniel.ist@gmail.com>
2021-05-08 14:46:32 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSucessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
@@ -28,4 +32,14 @@ public class SaleService {
 		// Para cada item da lista cria um novo objeto DTO e converte novamente para lista
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	@Transactional(readOnly = true) // não fazer lock no banco	
+	public List<SaleSumDTO> amountGroupBySeller() {
+		return repository.amountGroupBySeller();
+	}
+	
+	@Transactional(readOnly = true) // não fazer lock no banco	
+	public List<SaleSucessDTO> sucessGroupBySeller() {
+		return repository.sucessGroupBySeller();
+	}	
 }
